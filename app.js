b5aa5d06b4b3e97dc081fe9d3ee1db511147d5be
ilucheng <326186652@qq.com>
2017-07-21 00:16:58 +0800

    可以读取到test数据

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+const AV = require('./libs/av-weapp-min.js');
+
+// LeanCloud 应用的 ID 和 Key
+AV.init({
+  appId: 'XL7DVnWxLI9NLQ7v1Aujis6D-gzGzoHsz',
+  appKey: 'DSKLHVEOPzKy59UtLQbtyx0z',
+});
+
 //app.js
 App({})
